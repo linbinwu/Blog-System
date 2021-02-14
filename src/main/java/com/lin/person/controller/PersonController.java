df7fc1cec6e5135e9d0e0b23a3bd9582c29c0e7f
linbinwu <linbinwu@users.noreply.github.com>
2021-02-14 22:18:36 +0800

    src

--- src/main/java/com/lin/person/controller/PersonController.java
+++ src/main/java/com/lin/person/controller/PersonController.java
@@ -19,6 +19,7 @@ public class PersonController {
     @ResponseBody
     public List<Person> getAllPerson() {
         return personService.getAllPerson();
+
     }
 
     @RequestMapping("/addPerson")
